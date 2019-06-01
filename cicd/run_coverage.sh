6d4eb723c21b8b62cef757a29ed3821ef9b404ce
Markus Seidl <mark@markus-seidl.de>
2019-06-01 15:11:37 +0200

    Fix path brainfart.

--- cicd/run_coverage.sh
+++ cicd/run_coverage.sh
@@ -8,5 +8,5 @@ cp requirements.txt cicd/requirements.txt
 ${DOCKER} build cicd -f cicd/Dockerfile_python --tag pybutcherbackup-python
 rm cicd/requirements.txt
 
-${DOCKER} run --rm --name pybutcherbackup-python -v "$PWD":/usr/src/myapp -w /usr/src/myapp pybutcherbackup-python  "./cicd/run_coverage.sh"
+${DOCKER} run --rm --name pybutcherbackup-python -v "$PWD":/usr/src/myapp -w /usr/src/myapp pybutcherbackup-python  "./cicd/run_coverage_docker.sh"
 
