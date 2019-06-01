2543e7ada8d044b633ae71db45df966d7b326381
Markus Seidl <mark@markus-seidl.de>
2019-06-01 14:50:36 +0200

    Remove the requirement for a shell.

--- cicd/run_analyze_docker.sh
+++ cicd/run_analyze_docker.sh
@@ -4,4 +4,4 @@ DOCKER=docker
 
 ${DOCKER} build cicd -f cicd/Dockerfile_sonar --tag pybutcherbackup-sonar
 
-${DOCKER} run -it --rm --name pybutcherbackup-sonar -e SONAR_LOGIN=${SONAR_LOGIN} -v "$PWD":/usr/src/myapp -w /usr/src/myapp pybutcherbackup-sonar  "./cicd/run_analyze.sh"
+${DOCKER} run --rm --name pybutcherbackup-sonar -e SONAR_LOGIN=${SONAR_LOGIN} -v "$PWD":/usr/src/myapp -w /usr/src/myapp pybutcherbackup-sonar  "./cicd/run_analyze.sh"
--- cicd/run_coverage_docker.sh
+++ cicd/run_coverage_docker.sh
@@ -6,4 +6,4 @@ cp requirements.txt cicd/requirements.txt
 ${DOCKER} build cicd -f cicd/Dockerfile_python --tag pybutcherbackup-python
 rm cicd/requirements.txt
 
-${DOCKER} run -it --rm --name pybutcherbackup-python -v "$PWD":/usr/src/myapp -w /usr/src/myapp pybutcherbackup-python  "./run_coverage.sh"
+${DOCKER} run --rm --name pybutcherbackup-python -v "$PWD":/usr/src/myapp -w /usr/src/myapp pybutcherbackup-python  "./run_coverage.sh"
