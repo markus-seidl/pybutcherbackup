c3581cc6b8e416d5e20eddf9706e40f6fc6a3b52
Markus Seidl <mark@markus-seidl.de>
2020-08-02 16:18:11 +0200

    Minor fix.

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def cli_restore():
                                                  'Currently threading is *experimental*.', default=False)
 @click.option("--name", help="User name for that backup repository", default=None)
 @click.option("--terminal", help="Switch the progress type between SIMPLE, SILENT and TQDM",
-              type=click.Choice(['SIMPLE', 'SILENT', 'TQDM']), case_sensitive=False, default="TQDM")
+              type=click.Choice(['SIMPLE', 'SILENT', 'TQDM']), default="TQDM")
 @click.option("--dir-medium-size", help="Maximum size of a medium directory in GB", type=int, default=44)
 @click.option("--dummy", help="Internal Helper - do not use", type=bool, default=False)
 def action_backup(src: str, dest: str, index: str, passphrase: str, threading: bool, name: str, terminal: str,
